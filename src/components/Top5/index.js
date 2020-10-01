c84007ac00dc557cd7fd7eda569f694a0568e498
Ryan Albuquerque <ryan.albuquerque2002@gmail.com>
2020-09-30 22:22:08 -0400

    Mockando dados de Top5 iniciais

--- src/components/Top5/index.js
+++ src/components/Top5/index.js
@@ -9,8 +9,19 @@ export default function Top5() {
     const [top5, setTop5] = useState()
 
     useEffect(() => {
+        const inicialState = async () =>{
+            const response = await api.get('https://raw.githubusercontent.com/chaordic/developer-intern-challenge/master/Assets/urls.json')
+
+            response.data.sort((a,b) => (a.hits > b.hits) ? -1 : 1)
+
+            const top5 = response.data.slice(0,5)
+
+            setTop5(top5)
+
+        }
         const getTop = async()=>{
             const response = await api.get('/api/top5?total=5')
+            if(response.data.length < 5) inicialState()
             setTop5(response.data)
         }
         getTop()
@@ -26,7 +37,7 @@ export default function Top5() {
                 <tbody>
                     {top5?top5.map((e) => (
                         
-                        <tr key={e._id}>
+                        <tr key={e._id||e.id}>
                             <td><a target="blank" href={e.shortUrl}>{e.shortUrl}</a></td>
                             <td id="numberHits">{e.hits}</td>
                         </tr>
